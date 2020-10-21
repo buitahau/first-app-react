107f24adfb0edb7906ab23b0c0465debecc78fb8
buitahau@gmail.com <buitahau@gmail.com>
2020-10-21 07:11:39 +0700

    CurrencyConverter changed

--- src/components/currency_converter/CurrencyConverter.js
+++ src/components/currency_converter/CurrencyConverter.js
@@ -1,28 +1,30 @@
 import React from "react";
-import SelectorCurrency from "./SelectorCurrency.js";
 import GroupCurrency from "./GroupCurrency.js";
 
 class CurrencyConverter extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {val : 0};
+        this.state = {val : 0, currencyCode: "VND"};
         this.handleInputOnChange = this.handleInputOnChange.bind(this);
     }
 
-    handleInputOnChange(stateVal) {
-        console.log(stateVal);
+    handleInputOnChange(stateUpdated) {
+        this.setState({val: stateUpdated.val});
+        this.setState({currencyCode: stateUpdated.currencyCode});
     }
 
     render() {
-        const currentVal = this.state.val;
+        const currentState = this.state;
         return (
             <div className="currency_converter container">
                 <fieldset>
                     <legend>Currency Converter</legend>
                     <div className="row">
-                        <GroupCurrency onChange={this.handleInputOnChange}/>
-                        <GroupCurrency onChange={this.handleInputOnChange}/>
+                        <GroupCurrency onChange={this.handleInputOnChange} initValue={currentState} type="source" />
+                        <GroupCurrency onChange={this.handleInputOnChange} initValue={currentState} type="exchange" />
+                        <GroupCurrency onChange={this.handleInputOnChange} initValue={currentState} type="exchange" />
+                        <GroupCurrency onChange={this.handleInputOnChange} initValue={currentState} type="source" />
                     </div>
                 </fieldset>
             </div>
--- src/components/currency_converter/GroupCurrency.js
+++ src/components/currency_converter/GroupCurrency.js
@@ -2,10 +2,26 @@ import React from "react";
 
 class GroupCurrency extends React.Component {
 
+    static _currencies = [
+        {
+            code: "VND",
+            label: "VietNamDong",
+            ratio: 1
+        },
+        {
+            code: "USD",
+            label: "Dola My",
+            ratio: 21000
+        }
+    ];
+
+    static SOURCE = "source";
+    static EXCHANGE = "exchange";
+
     constructor(props) {
         super(props);
 
-        this.state = {currencyCode: "VND", val: 0};
+        this.state = GroupCurrency.initStateFromProps(props);
 
         this.getListOptionCurrency = this.getListOptionCurrency.bind(this);
         this.handleInputOnChangeInternal = this.handleInputOnChangeInternal.bind(this);
@@ -16,52 +32,92 @@ class GroupCurrency extends React.Component {
     handleSelectOnChangeInternal(e) {
         const selectVal = e.target.value;
         this.setState({currencyCode: selectVal});
-        console.log(selectVal);
 
-        let stateVal = this.state;
-        stateVal.currencyCode = selectVal;
+        if (this.state.type == GroupCurrency.SOURCE) {
+            let stateVal = this.state;
+            stateVal.currencyCode = selectVal;
 
-        this.passValueToParent(stateVal);
+            this.passValueToParent(stateVal);
+        }
     }
 
     handleInputOnChangeInternal(e) {
         const inputVal = e.target.value;
         this.setState({val: inputVal});
 
-        let stateVal = this.state;
-        stateVal.val = inputVal;
+        if (this.state.type == GroupCurrency.SOURCE) {
+            let stateVal = this.state;
+            stateVal.val = inputVal;
 
-        this.passValueToParent(stateVal);
+            this.passValueToParent(stateVal);
+        }
     }
 
-    passValueToParent(stateVal) {
-
-        this.props.onChange(stateVal);
+    passValueToParent(stateUpdated) {
+        this.props.onChange(stateUpdated);
     }
 
     getListOptionCurrency() {
-        const currencies = [
-            {
-                code: "VND",
-                label: "VietNamDong"
-            },
-            {
-                code: "USD",
-                label: "Dola My"
-            }
-        ];
-        return currencies.map(currency => {
-            return <option key={currency.code} value={currency.code}>{currency.label}</option>
+        return GroupCurrency._currencies.map(currency => {
+            return <option key={currency.code}
+                           value={currency.code}>{currency.label}</option>
         });
+    }
+
+    static exchangeMoney(currentCode, initializeCode, initializeMoney) {
+        const currentRatio = GroupCurrency.getRatioFromCode(currentCode);
+        const initializeRatio = GroupCurrency.getRatioFromCode(initializeCode);
+
+        return (initializeMoney / currentRatio) * initializeRatio;
+    }
+
+    static getDerivedStateFromProps(props, currentState) {
+
+        return {
+            val: GroupCurrency.exchangeMoney(currentState.currencyCode,
+                props.initValue.currencyCode, props.initValue.val)
+        };
+    }
+
+    static getRatioFromCode(code) {
+        return GroupCurrency._currencies.filter(
+            currency => currency.code == code)
+            [0].ratio;
+    }
+
+    static initStateFromProps(props) {
+        let state = {
+            currencyCode: "VND", val: 0
+        };
+
+        let type = GroupCurrency.SOURCE;
+        if(props.type != GroupCurrency.SOURCE) {
+            type = GroupCurrency.EXCHANGE;
+        }
+        state.type = type;
+
+        return state;
     }
 
     render() {
         return (
-            <div className="col-sm-6">
-                <input type="text" onChange={this.handleInputOnChangeInternal} />
-                <select className="selector_currency" onChange={this.handleSelectOnChangeInternal}>
-                    {this.getListOptionCurrency()}
-                </select>
+            <div className="row">
+                <div className="col-sm-12">
+                    <fieldset>
+                        <legend>{this.state.type}</legend>
+                        <div className="col-sm-6">
+                            <input type="text"
+                                   onChange={this.handleInputOnChangeInternal}
+                                   value={this.state.val}/>
+                        </div>
+                        <div className="col-sm-6">
+                            <select className="selector_currency"
+                                    onChange={this.handleSelectOnChangeInternal}>
+                                {this.getListOptionCurrency()}
+                            </select>
+                        </div>
+                    </fieldset>
+                </div>
             </div>
         );
     }
